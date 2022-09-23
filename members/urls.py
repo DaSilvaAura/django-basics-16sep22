67e8635e9d8c5b535b85cee93d2da86b539ceb8a
DasilvaAura <114219580+DaSilvaAura@users.noreply.github.com>
2022-09-23 12:04:57 +0000

    Update records

--- members/urls.py
+++ members/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+
 from . import views
 
 urlpatterns = [
@@ -6,4 +7,6 @@ urlpatterns = [
   path('add/', views.add, name='add'),
   path('add/addrecord/', views.addrecord, name='addrecord'),
   path('delete/<int:id>', views.delete, name='delete'),
+  path('update/<int:id>', views.update, name='update'),
+  path('update/updaterecord/<int:id>', views.updaterecord, name='updaterecord'),
 ]
